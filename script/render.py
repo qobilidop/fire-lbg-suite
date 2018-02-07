2905c547d7e8496cdd34b86ba368a78e3513fd07
qobilidop <qobilidop@gmail.com>
2018-02-07 14:47:32 -0800

    Improve profile system

--- script/render.py
+++ script/render.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 from argparse import ArgumentParser
+import collections
 from pathlib import Path
+import pprint
 
 from jinja2 import Environment
 from jinja2 import FileSystemLoader
@@ -10,27 +12,50 @@ import yaml
 def main():
     # Parse arguments
     parser = ArgumentParser()
-    parser.add_argument('config_file', type=Path)
-    parser.add_argument('--output_dir', type=Path, default='run')
+    parser.add_argument('profile', type=Path)
     args = parser.parse_args()
-    config_file = args.config_file
-    output_dir = args.output_dir
 
-    # Load config file
-    with config_file.open() as f:
-        config = yaml.load(f)
+    # Load profile
+    profile = load_profile(args.profile)
+    print('Applying the following profile:')
+    pprint.pprint(profile)
 
     # Render template into target
-    target_dir = output_dir / config['name']
-    template_dir = config_file.parent / config['template']
+    repo_dir = Path(__file__).resolve().parents[1]
+    target_dir = repo_dir / 'run' / profile['name']
+    template_dir = repo_dir / 'template'
     env = Environment(loader=FileSystemLoader(str(template_dir)))
     env.keep_trailing_newline = True
     for template_name in env.list_templates():
         template = env.get_template(template_name)
-        rendered = template.render(config)
+        rendered = template.render(profile)
         target = target_dir / template_name
         target.parent.mkdir(parents=True, exist_ok=True)
         target.write_text(rendered)
+    print(f'Template is rendered into {target_dir}.')
+
+
+def load_profile(path: Path):
+    """Load profile dictionary."""
+    with path.open() as f:
+        profile = yaml.load(f)
+    if 'import' in profile:
+        path_import = path.parent / profile['import']
+        del profile['import']
+        print(f'Importing {path_import}')
+        profile_to_import = load_profile(path_import)
+        profile = update_profile(profile_to_import, profile)
+    return profile
+
+
+def update_profile(base, patch):
+    """Update profile dictionary recursively."""
+    for k, v in patch.items():
+        if k in base and isinstance(v, collections.Mapping):
+            update_profile(base[k], v)
+        else:
+            base[k] = v
+    return base
 
 
 if __name__ == '__main__':
