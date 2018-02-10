c67dc56da884940f4ec8dab0948cc409362aff79
qobilidop <qobilidop@gmail.com>
2018-02-10 10:30:50 -0800

    Install two versions of AHF & add the ahf script

--- install/ahf.sh
+++ install/ahf.sh
@@ -2,6 +2,7 @@
 
 cd $(dirname $0)
 cd .. && source init.sh && cd install
+AHF_DM_PATCH="$PWD/ahf_dm_Makefile.config.patch"
 AHF_PATCH="$PWD/ahf_Makefile.config.patch"
 
 # Download source code
@@ -9,9 +10,18 @@ mkdir -p $REPO_PREFIX/src
 cd $REPO_PREFIX/src
 curl http://popia.ft.uam.es/AHF/files/ahf-v1.0-094.tgz | tar -vxz
 cd ahf-v1.0-094
-patch < $AHF_PATCH
 
 # Install
 mkdir -p $REPO_PREFIX/bin
+# AHF_DM
+patch < $AHF_DM_PATCH
+make AHF
+mv bin/AHF-v1.0-094 $REPO_PREFIX/bin/AHF_DM
+patch -R < $AHF_DM_PATCH
+make clean
+# AHF
+patch < $AHF_PATCH
 make AHF
 mv bin/AHF-v1.0-094 $REPO_PREFIX/bin/AHF
+patch -R < $AHF_PATCH
+make clean
