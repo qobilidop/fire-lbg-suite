3f04974c421381259fb10f9f267bfd102de743e8
qobilidop <qobilidop@gmail.com>
2018-03-30 12:04:16 -0700

    Another try to fix the hdf5 version mismatch issue by relying on conda

--- init-bridges.sh
+++ init-bridges.sh
@@ -4,7 +4,7 @@
 module purge
 module load psc_path slurm/17.02.5
 module load intel/17.4 mpi/intel_mpi
-module load fftw3/3.3.4 hdf5/1.8.16_intel
+module load fftw3/3.3.4
 
 # Compiler settings
 export RUN_CC=icc
@@ -21,8 +21,6 @@ export RUN_FFTW3_INC="$FFTW3_INCLUDE"
 export RUN_FFTW3_LIB="$FFTW3_LIB"
 export RUN_GSL_INC="/usr/include/gsl"
 export RUN_GSL_LIB="/lib64"
-export RUN_HDF5_INC="$HDF5_ROOT/include"
-export RUN_HDF5_LIB="$HDF5_ROOT/lib"
 export RUN_MKL_INC="$MKLROOT/include"
 export RUN_MKL_LIB="$MKLROOT/lib"
 
--- init-general.sh
+++ init-general.sh
@@ -2,6 +2,8 @@
 source activate $PWD/src/conda/env
 export RUN_FFTW2_INC="$CONDA_PREFIX/include"
 export RUN_FFTW2_LIB="$CONDA_PREFIX/lib"
+export RUN_HDF5_INC="$CONDA_PREFIX/include"
+export RUN_HDF5_LIB="$CONDA_PREFIX/lib"
 
 # Export PATH
 export PATH=$PWD/src/script:$PATH
