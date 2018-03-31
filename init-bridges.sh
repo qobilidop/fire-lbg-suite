b202c940857a285b0aff50390fa1263956085826
qobilidop <qobilidop@gmail.com>
2018-03-30 18:38:27 -0700

    Change naming convention

--- init-bridges.sh
+++ init-bridges.sh
@@ -7,21 +7,21 @@ module load intel/17.4 mpi/intel_mpi
 module load fftw3/3.3.4
 
 # Compiler settings
-export RUN_CC=icc
-export RUN_CXX=icpc
-export RUN_FC=ifort
-export RUN_MPICC=mpiicc
-export RUN_MPICXX=mpiicpc
-export RUN_MPIFC=mpiifort
-export RUN_OPT_MPI="-mt_mpi"
-export RUN_OPT_OMP="-qopenmp"
+export MY_CC=icc
+export MY_CXX=icpc
+export MY_FC=ifort
+export MY_MPICC=mpiicc
+export MY_MPICXX=mpiicpc
+export MY_MPIFC=mpiifort
+export MY_OPT_MPI="-mt_mpi"
+export MY_OPT_OMP="-qopenmp"
 
 # System packages
-export RUN_FFTW3_INC="$FFTW3_INCLUDE"
-export RUN_FFTW3_LIB="$FFTW3_LIB"
-export RUN_GSL_INC="/usr/include/gsl"
-export RUN_GSL_LIB="/lib64"
-export RUN_MKL_INC="$MKLROOT/include"
-export RUN_MKL_LIB="$MKLROOT/lib"
+export MY_FFTW3_INC="-I$FFTW3_INCLUDE"
+export MY_FFTW3_LIB="-L$FFTW3_LIB"
+export MY_GSL_INC="-I/usr/include/gsl"
+export MY_GSL_LIB="-L/lib64"
+export MY_MKL_INC="-I$MKLROOT/include"
+export MY_MKL_LIB="-L$MKLROOT/lib"
 
 source init-general.sh
--- init-general.sh
+++ init-general.sh
@@ -1,9 +1,11 @@
 # Conda packages
 source activate $PWD/src/conda/env
-export RUN_FFTW2_INC="$CONDA_PREFIX/include"
-export RUN_FFTW2_LIB="$CONDA_PREFIX/lib"
-export RUN_HDF5_INC="$CONDA_PREFIX/include"
-export RUN_HDF5_LIB="$CONDA_PREFIX/lib"
+CONDA_INC="-I$CONDA_PREFIX/include"
+CONDA_LIB="-Wl,-rpath,$CONDA_PREFIX/lib -L$CONDA_PREFIX/lib"
+export MY_FFTW2_INC=$CONDA_INC
+export MY_FFTW2_LIB=$CONDA_LIB
+export MY_HDF5_INC=$CONDA_INC
+export MY_HDF5_LIB=$CONDA_LIB
 
 # Export PATH
-export PATH=$PWD/src/script:$PATH
+export PATH="$PWD/src/script:$PATH"
