5cd438e2e1cf1a2139d410510e08b7bc3b195ffc
qobilidop <qobilidop@gmail.com>
2018-04-09 09:45:45 -0700

    Capture benchmark results

--- src/script/gen_bench_results.py
+++ src/script/gen_bench_results.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python
 from pathlib import Path
+import re
 
 
 with Path('results.csv').open('w') as f:
-    f.write('run, cpu, wall_time_h, cpu_time_h\n')
+    f.write('run,nodes,threads,cpus,wall_time_h,cpu_time_h\n')
     for output in sorted(Path('.').glob('*/output')):
+        print(output)
         run = output.parent.name
-        nodes = int(run.split('_')[0][1:])
+        nodes, threads = re.match(f'n([0-9]+)_t([0-9]+)', run).group(1, 2)
+        nodes = int(nodes)
+        threads = int(threads)
         cpus = nodes * 28
         ctimes = [p.stat().st_ctime for p in output.glob('*')
                   if not p.is_symlink()]
         wall_time_s = max(ctimes) - min(ctimes)
         wall_time_h = wall_time_s / 3600
         cpu_time_h = cpus * wall_time_h
-        f.write(f'{run}, {cpus}, {wall_time_h:.1f}, {cpu_time_h:.1f}\n')
+        f.write(f'{run},{nodes},{threads},{cpus},{wall_time_h:.2f},{cpu_time_h:.2f}\n')
