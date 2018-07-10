ec174fc06ceae3ec2e0079fb1f11454b1d1dd4a2
qobilidop <qobilidop@gmail.com>
2018-07-10 12:23:36 -0700

    Fix job script

--- lab/1-sample-selection/job/bridges-box-ahf.sh
+++ lab/1-sample-selection/job/bridges-box-ahf.sh
@@ -2,12 +2,13 @@
 #SBATCH -J box-ahf
 #SBATCH -p RM
 #SBATCH -N 1
-#SBATCH --ntasks-per-node 28
+#SBATCH --ntasks-per-node=28
 #SBATCH -t 4:00:00
 #SBATCH --export=ALL
+#SBATCH -o "%x.%j.log"
 set -x
 
 cd "$PROJECT_DIR"
-source bin/activate
+source env/activate
 
 ./code/script/box-ahf.py
