a7583a7feba32967954d7b8d8a170ee4b1845e44
qobilidop <qobilidop@gmail.com>
2018-07-11 16:13:55 -0700

    Update AHF (un)installer

--- code/extern/ahf/install.sh
+++ code/extern/ahf/install.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 set -e
 
-# Download
 mkdir -p "$LOCAL_PREFIX/opt"
 cd "$LOCAL_PREFIX/opt"
-curl http://popia.ft.uam.es/AHF/files/ahf-v1.0-094.tgz | tar xz
+
+# Download
+source=http://popia.ft.uam.es/AHF/files/ahf-v1.0-094.tgz
+curl "$source" | tar xz
 
 # Patch
 cd ahf-v1.0-094
-patch Makefile "$PROJECT_DIR/code/extern/ahf/Makefile.patch"
+patch < "$PROJECT_DIR/code/extern/ahf/Makefile.patch"
 
 # Configure
 export CC="$LOCAL_CC"
@@ -19,17 +21,17 @@ export LNFLAGS="$LOCAL_LDFLAGS"
 export DEFINEFLAGS=
 export MAKE=make
 
-# Compile AHF-dmo for dark matter only simulations
+# Compile
+## Compile AHF-dmo for dark matter only simulations
 make clean
 export DEFINEFLAGS="-DMULTIMASS -DWITH_OPENMP"
 export AHF_BIN="AHF-dmo"
-make AHF
-
-# Compile AHF for hydro simulations
+make -j AHF
+## Compile AHF for hydro simulations
 make clean
 export DEFINEFLAGS="-DMULTIMASS -DGAS_PARTICLES -DREFINE_BARYONIC_MASS -DWITH_OPENMP"
 export AHF_BIN="AHF"
-make AHF
+make -j AHF
 
-# Finalize
+# Install
 mv bin/* "$LOCAL_PREFIX"/bin/
---
+++ code/extern/ahf/uninstall.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+rm -rf "$LOCAL_PREFIX/opt/ahf-v1.0-094" "$LOCAL_PREFIX"/bin/{AHF,AHF-dmo}
