d6fa407143b38c4a64a291cd77d1b35f2c2bfc36
qobilidop <qobilidop@gmail.com>
2018-07-11 11:42:18 -0700

    Fix variable name

--- code/bin/deploy.sh
+++ code/bin/deploy.sh
@@ -4,9 +4,10 @@ host="$1"
 
 opt=(-amvz --update --delete-after --filter ":- .gitignore" --exclude ".git")
 src="$PROJECT_DIR/"
-dst="$host:~/project/$PROJECT/"
+dst="$host:~/project/$PROJECT_NAME/"
 cmd=(rsync "${opt[@]}" "$src" "$dst")
 
+echo "${cmd[*]}"
 "${cmd[@]}" --dry-run
 echo "Run?"
 select yn in "Yes" "No"; do
