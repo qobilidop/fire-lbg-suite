2a4e1f06c10bb08eae9cf5ce323ad83eff3de6bd
qobilidop <qobilidop@gmail.com>
2018-07-12 14:28:24 -0700

    Update conda env using `conda update`

--- env/conda/init.sh
+++ env/conda/init.sh
@@ -2,6 +2,4 @@
 set -e
 
 source enable-conda
-conda activate "$LOCAL_CONDA_PREFIX"
-conda install -y gcc openmpi
-conda install -y fftw=3 gsl hdf5
+conda env update -f "$PROJECT_DIR/env/conda/environment.yml" -p "$LOCAL_CONDA_PREFIX"
