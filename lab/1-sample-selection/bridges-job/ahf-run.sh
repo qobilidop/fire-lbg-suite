79aa3836eca5df457853bb0a9d45775a32ea673c
qobilidop <qobilidop@gmail.com>
2018-07-13 00:45:53 -0700

    Move AHF jobs from TSCC to Bridges
    I can't make it work on TSCC, and it's taking me too much time.

---
+++ lab/1-sample-selection/bridges-job/ahf-run.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#SBATCH -J box-ahf
+#SBATCH -p RM
+#SBATCH -N 2
+#SBATCH --ntasks-per-node=4
+#SBATCH --cpus-per-task=7
+#SBATCH -t 8:00:00
+#SBATCH --export=ALL
+#SBATCH -o "%x.%j.log"
+set -e
+
+cd "$PROJECT_DIR"
+source env/activate
+check-local-env bridges
+
+cd data/box-ahf
+export I_MPI_JOB_RESPECT_PROCESS_PLACEMENT=0
+MPI_OPT="-print-rank-map -n $SLURM_NTASKS -ppn $SLURM_NTASKS_PER_NODE"
+MPI_OPT="$MPI_OPT -genv OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK"
+MPI_OPT="$MPI_OPT -genv I_MPI_PIN_DOMAIN=omp"
+mpirun $MPI_OPT AHF-dmo-mpi snapshot_005.input
---
+++ lab/1-sample-selection/bridges-job/ahf-setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#SBATCH -J box-ahf
+#SBATCH -p RM
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=28
+#SBATCH -t 4:00:00
+#SBATCH --export=ALL
+#SBATCH -o "%x.%j.log"
+set -e
+
+cd "$PROJECT_DIR"
+source env/activate
+check-local-env bridges
+
+./lab/1-sample-selection/script/ahf-setup.py
