f6f7d1479ddd12008e62901180a3e80876993865
qobilidop <qobilidop@gmail.com>
2018-10-15 04:45:41 -0700

    Simplify environment management
    Now we assume galenv is installed globally.

--- env/init.sh
+++ env/init.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash
 set -e
 
+if [ -z "$PROJECT_ROOT" ]; then
+    echo "PROJECT_ROOT is not set. Activate first:"
+    echo ">>> source env/activate"
+    exit
+fi
 cd "$PROJECT_ROOT"
-./env/init-galenv.sh
-./env/init-conda.sh
+source env/activate
+
+echo "Initializing conda env"
+source env/enable-conda
+conda env create -f env/environment.yml -p "$PROJECT_CONDA"
+source env/activate
+if [ ! -z "$GALENV_MPICC" ]; then
+    # Install mpi4py using local MPI
+    env MPICC="$GALENV_MPICC" pip install mpi4py
+fi
+(
+    cd code
+    pip install -e .
+)
+conda clean -ay
