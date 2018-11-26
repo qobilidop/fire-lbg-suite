3b05dfb8c858155c0309424c5f9438edec798305
qobilidop <qobilidop@gmail.com>
2018-11-25 21:47:04 -0800

    Switch to use gizenv

---
+++ env/activate.sh
@@ -0,0 +1,1 @@
+source ~/gizenv/bin/gizenv-activate.sh
--- env/update.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-cd "$PROJECT_ROOT"
-
-echo "Updating conda env"
-source env/enable-conda
-conda env update -f env/environment.yml -p "$PROJECT_CONDA"
-source env/activate
-conda clean -ay
