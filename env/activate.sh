a17d345d251e5119e69f4f07b99b9a6e32329c5d
qobilidop <qobilidop@gmail.com>
2018-11-30 13:42:39 -0800

    Make activation script more robust

--- env/activate.sh
+++ env/activate.sh
@@ -1,4 +1,12 @@
-export PROJECT_ACTIVATE="$PWD/env/activate.sh"
+# Only Bash and Zsh are supported
+if [ -n "$BASH_VERSION" ]; then
+    this_file="${BASH_SOURCE[0]}"
+elif [ -n "$ZSH_VERSION" ]; then
+    this_file="${(%):-%x}"
+fi
+export PROJECT_ACTIVATE="$this_file"
+this_dir="$(dirname "$this_file")"
+PROJECT_ROOT="$(cd "$this_dir/.." >/dev/null && pwd)"
 
 source ~/gizenv/bin/gizenv-activate.sh
-export PATH="$PWD/script:$PATH"
+export PATH="$PROJRCT_ROOT/script:$PATH"
