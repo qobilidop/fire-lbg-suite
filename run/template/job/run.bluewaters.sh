a4d37150159fdc41c6b25cad10d8ee0eeb641d75
qobilidop <qobilidop@gmail.com>
2019-03-07 15:07:26 -0800

    Add run job template for bluewaters

---
+++ run/template/job/run.bluewaters.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#PBS -q {{ QUEUE }}
+#PBS -l nodes={{ NODES }}:ppn={{ CORES }}:xe
+#PBS -l walltime={{ HOUR }}:00:00
+#PBS -j oe
+#PBS -o run.log
+#PBS -V
+#PBS -d .
+set -e
+cd ..
+pwd
+source gizenv-activate.sh
+
+export OMP_NUM_THREADS={{ OMP }}
+OPT="-N {{ MPI }} -d {{ OMP }}"
+
+date
+if [[ -d output/restartfiles ]]; then
+    # Restart
+    aprun $OPT ./GIZMO gizmo_params.txt 1
+else
+    # Start from scratch
+    aprun $OPT ./GIZMO gizmo_params.txt
+fi
+date
