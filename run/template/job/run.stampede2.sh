acbd62377dda3a99912490d27be3bc99e2bf6204
qobilidop <qobilidop@gmail.com>
2019-03-07 21:30:50 -0800

    Set up h01 run on Stampede2

---
+++ run/template/job/run.stampede2.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH --job-name={{ NAME }}
+#SBATCH --partition={{ QUEUE }}
+#SBATCH --nodes={{ NODES }}
+#SBATCH --ntasks-per-node={{ MPI }}
+#SBATCH --cpus-per-task={{ OMP }}
+#SBATCH --time={{ HOUR }}:00:00
+#SBATCH --account=TG-AST140023
+#SBATCH --output=run.log
+#SBATCH --export=ALL
+#SBATCH --workdir=.
+# Reference: https://portal.tacc.utexas.edu/user-guides/stampede2#job-scripts
+set -e
+cd ..
+pwd
+source gizenv-activate.sh
+
+if [[ -f output/snapshot_190.hdf5 || -d output/snapdir_190 ]]; then
+    echo "job finished"
+    exit
+else
+    sbatch --dependency=afterok:"$SLURM_JOB_ID" run.sh
+fi
+
+export OMP_NUM_THREADS="$SLURM_CPUS_PER_TASK"
+OPT="tacc_affinity"
+
+date
+if [[ -d output/restartfiles ]]; then
+    # Restart
+    ibrun $OPT ./GIZMO gizmo_params.txt 1
+else
+    # Start from scratch
+    ibrun $OPT ./GIZMO gizmo_params.txt
+fi
+date
