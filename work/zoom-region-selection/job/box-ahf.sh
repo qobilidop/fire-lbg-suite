dd1100c9223e0dac9b7ab01d19041cf8d084e599
qobilidop <qobilidop@gmail.com>
2019-05-17 10:48:45 -0700

    Fix box AHF job

--- work/zoom-region-selection/job/box-ahf.sh
+++ work/zoom-region-selection/job/box-ahf.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
-# qsub job/box-ahf-run.sh
+# qsub job/box-ahf.sh
+# https://www.sdsc.edu/support/user_guides/tscc.html#submit-job
 #PBS -N box-ahf-run
 #PBS -q condo
 #PBS -l nodes=4:ppn=16:ib
 #PBS -l walltime=8:00:00
 #PBS -j oe
-#PBS -o job/box-ahf-run.log
+#PBS -o job/box-ahf.log
+#PBS -V
 #PBS -d .
 set -e
 spack env activate gizmo
+spack env status
 set -x
 
 export OMP_NUM_THREADS=4
-MPIRUN="mpirun -machinefile $PBS_NODEFILE -npernode 4"
+MPIRUN="mpirun -v -machinefile $PBS_NODEFILE -npernode 4 -x PATH"
 
 cd data/box/ahf
 pwd
