0a11442d338d6b94493a7c0787e39b90937b03fe
qobilidop <qobilidop@gmail.com>
2019-05-17 11:06:13 -0700

    Reselect halo sample

---
+++ work/zoom-region-selection/select-sample.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+"""Process AHF halos to filter out a short list of interested halos."""
+import numpy as np
+import pandas as pd
+
+
+CAND_FILE = 'data/halo/candidate.csv'
+MENV_FILE = 'data/halo/menv.csv'
+OUTPUT = 'data/halo/sample.csv'
+M_MAX = 10**12.2  # Msun
+M_MIN = 10**11.8  # Msun
+RANDOM_SEED_1 = 42
+RANDOM_SEED_2 = 1
+
+
+if __name__ == '__main__':
+    # Read input
+    cand = pd.read_csv(CAND_FILE)
+    menv = pd.read_csv(MENV_FILE)
+    cand['Menv'] = menv['Menv']
+
+    # Keep isolated halos only
+    cand = cand[cand['isolated']]
+
+    # Select sample
+    x = np.log10(cand['Mvir'].values)
+    y = np.log10(cand['Menv'].values)
+    x_min, x_max = np.log10(M_MIN), np.log10(M_MAX)
+    i, xbins = pd.cut(x, np.linspace(x_min, x_max, 3), labels=False, retbins=True)
+    j, ybins = pd.qcut(y, 4, labels=False, retbins=True)
+    cand = cand.assign(i=i)
+    cand = cand.assign(j=j)
+    sample = cand.groupby(['i', 'j']).apply(
+        lambda df: df.sample(random_state=RANDOM_SEED_1)
+    )
+
+    # Reroll the dice for h12 and h13, which are too massive to finish
+    # realistically with the computing resources we have
+    sample_again = cand.groupby(['i', 'j']).apply(
+        lambda df: df.sample(random_state=RANDOM_SEED_2)
+    )
+    for i in [6, 7]:
+        sample.iloc[i] = sample_again.iloc[i]
+
+    # Name the halos
+    names = []
+    for i, j in zip(sample['i'], sample['j']):
+        names += [f'h{i}{j}']
+    sample['name'] = names
+
+    # Save output
+    sample = sample[['name', 'Mvir', 'Menv', 'Rvir', 'Xc', 'Yc', 'Zc']]
+    sample.to_csv(OUTPUT, index=False)
