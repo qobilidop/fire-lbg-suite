dfdbf780f2218c1d54ab58ac71600c27705b2b83
qobilidop <qobilidop@gmail.com>
2019-05-29 09:16:42 -0700

    Restart h00 from restartfiles

--- run/z2m12_h00_ref13/job/run.sh
+++ run/z2m12_h00_ref13/job/run.sh
@@ -21,7 +21,7 @@ MPIRUN="ibrun"
 if [[ -d output/restartfiles ]]; then
     RESTART_FLAG=1
 else
-    RESTART_FLAG=2
+    RESTART_FLAG=
 fi
 
 eval "$MPIRUN" ./GIZMO gizmo_params.txt "$RESTART_FLAG"
