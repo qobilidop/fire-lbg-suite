adc194d09c3e78116722624b63db4848e27aef36
qobilidop <qobilidop@gmail.com>
2019-10-24 09:10:22 -0700

    Move simulations among HPC sites

--- run/z2m12_h11_ref13/job/run.sh
+++ run/z2m12_h11_ref13/job/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -J h11-run
 #SBATCH -d singleton
-#SBATCH -p RM
+#SBATCH -p skx-normal
 #SBATCH -N 64
-#SBATCH --ntasks-per-node=14
+#SBATCH --ntasks-per-node=24
 #SBATCH --cpus-per-task=2
 #SBATCH -t 48:00:00
 #SBATCH -o job/run.log
@@ -14,8 +14,8 @@ spack env activate fire-lbg-suite
 set -x
 pwd
 
-export I_MPI_JOB_RESPECT_PROCESS_PLACEMENT=0
-MPIRUN="mpirun -n $SLURM_NTASKS -ppn 14 -genv OMP_NUM_THREADS=2 -genv I_MPI_PIN_DOMAIN=omp"
+export OMP_NUM_THREADS=2
+MPIRUN="ibrun"
 if [[ -d output/restartfiles ]]; then
     RESTART_FLAG=1
 else
